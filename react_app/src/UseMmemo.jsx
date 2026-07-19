508c2d3087abf88243cfeeb29d59a93125d226df
Ankit Kumar <ankitkumarswe01@gmail.com>
2026-07-19 17:08:38 +0530

    updated

---
+++ react_app/src/UseMmemo.jsx
@@ -0,0 +1,15 @@
+function useMemo() {
+    function ProductList({ products, searchTerm }) {
+  // This runs on EVERY render, even if searchTerm didn't change
+  const filtered = products.filter(p => p.name.includes(searchTerm));
+  return <ul>{filtered.map(p => <li key={p.id}>{p.name}</li>)}</ul>;
+}
+  
+  return (
+    <>
+      <h1>Use of UseMemo</h1>
+    </>
+  );
+}
+
+export default useMemo;
